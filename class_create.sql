6b20bbfe5e1a23f3da9ca899f23e32d3d03e00a2
bolicoding <bolicoding@gmail.com>
2020-11-22 17:46:58 -0700

    added courses

---
+++ class_create.sql
@@ -0,0 +1,19 @@
+use school_schema;
+
+INSERT INTO course(course_name)
+VALUES("English");
+
+INSERT INTO course(course_name)
+VALUES("Math");
+
+INSERT INTO course(course_name)
+VALUES("Computer Science");
+
+INSERT INTO course(course_name)
+VALUES("Poetry");
+
+INSERT INTO course(course_name)
+VALUES("Chemistry");
+
+INSERT INTO course(course_name)
+VALUES("Sewing");
