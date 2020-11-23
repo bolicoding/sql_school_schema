69e92e0ddc3a54b77ca09714334a25ede6718524
bolicoding <bolicoding@gmail.com>
2020-11-22 17:45:41 -0700

    prof creation

---
+++ professor_create.sql
@@ -0,0 +1,10 @@
+use school_schema;
+
+INSERT INTO professor(professor_name)
+VALUES("Mr. Floyd");
+
+INSERT INTO professor(professor_name)
+VALUES("Mr. McKelvey");
+
+INSERT INTO professor(professor_name)
+VALUES("Mr. Suess");
