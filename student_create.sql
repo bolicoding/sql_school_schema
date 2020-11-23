0eee2f195b93141ad9f837f75972bb7e77fc5852
bolicoding <bolicoding@gmail.com>
2020-11-22 17:44:52 -0700

    student creation

---
+++ student_create.sql
@@ -0,0 +1,16 @@
+use school_schema;
+
+INSERT INTO student(student_name)
+VALUES("Boli Mon");
+
+INSERT INTO student(student_name)
+VALUES("Mili Bust");
+
+INSERT INTO student(student_name)
+VALUES("Chels Oro");
+
+INSERT INTO student(student_name)
+VALUES("Ashley Oro");
+
+INSERT INTO student(student_name)
+VALUES("Diego De");
