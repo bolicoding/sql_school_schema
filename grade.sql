2b95e5e27d4822d358d4aaf9bbdc8aedd38a3fde
bolicoding <bolicoding@gmail.com>
2020-11-22 17:47:54 -0700

    added grade table info

---
+++ grade.sql
@@ -0,0 +1,46 @@
+use school_schema;
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(86, 1, 1, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(68, 1, 2, 2);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(79, 1, 3, 3);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(92, 2, 3, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(88, 2, 5, 2);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(77, 2, 3, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(83, 3, 6, 3);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(81, 3, 4, 3);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(76, 3, 2, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(86, 4, 4, 2);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(86, 4, 2, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(56, 4, 3, 3);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(86, 5, 1, 1);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(82, 5, 2, 2);
+
+INSERT INTO grade(grade, student_student_id, course_course_id, professor_professor_id)
+VALUES(84, 5, 3, 3);
